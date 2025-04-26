d08f2ae9565fc7d6fb70896def4d367e6ae2c22d
Saif <sasi@rrc.ca>
2025-04-26 14:35:05 -0500

    Require mutex_m and logger in boot to avoid ActiveSupport load errors

--- config/boot.rb
+++ config/boot.rb
@@ -1,4 +1,6 @@
 ENV["BUNDLE_GEMFILE"] ||= File.expand_path("../Gemfile", __dir__)
 
-require "bundler/setup" # Set up gems listed in the Gemfile.
+require 'mutex_m'        # ensure ActiveSupport notifications thread safe
+require 'logger'         # ensure LoggerThreadSafeLevel finds ::Logger
+require "bundler/setup"  # Set up gems listed in the Gemfile.
 require "bootsnap/setup" # Speed up boot time by caching expensive operations.
